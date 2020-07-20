ddc72cfcc1a6ec035b2b62e6d342bf7fde505b4a
Bryan Dollery <bryan.dollery@gmail.com>
2020-07-20 11:12:16 +0300

    mostly working

--- lab/main.tf
+++ lab/main.tf
@@ -1,20 +1,46 @@
-module "tag_generator" {
+module "tags_network" {
   source      = "git::https://github.com/cloudposse/terraform-null-label.git"
-  namespace   = format("kh-lab-%s", var.name)
-  environment = var.name
-  name        = format("DevOps-Bootcamp-%s", var.name)
-  attributes  = ["public"]
+  namespace   = var.name
+  environment = "dev"
+  name        = "devops-bootcamp"
+  attributes  = ["network"]
   delimiter   = "_"
 }
 
+module "tags_bastion" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git"
+  namespace   = var.name
+  environment = "dev"
+  name        = "devops-bootcamp"
+  attributes  = ["bastion"]
+  delimiter   = "_"
+}
+
+module "tags_worker" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git"
+  namespace   = var.name
+  environment = "dev"
+  name        = "devops-bootcamp"
+  attributes  = ["worker"]
+  delimiter   = "_"
+}
+
+module "tags_controlplane" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git"
+  namespace   = var.name
+  environment = "dev"
+  name        = "devops-bootcamp"
+  attributes  = ["controlplane"]
+  delimiter   = "_"
+}
 resource "aws_vpc" "lab" {
   cidr_block = "10.0.0.0/16"
-  tags       = module.tag_generator.tags
+  tags       = module.tags_network.tags
 }
 
 resource "aws_internet_gateway" "lab_gateway" {
   vpc_id = aws_vpc.lab.id
-  tags   = module.tag_generator.tags
+  tags   = module.tags_network.tags
 }
 
 resource "aws_route" "lab_internet_access" {
@@ -23,42 +49,71 @@ resource "aws_route" "lab_internet_access" {
   gateway_id             = aws_internet_gateway.lab_gateway.id
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_subnet" "bastion" {
   vpc_id                  = aws_vpc.lab.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[0]
-  tags                    = module.tag_generator.tags
+  tags                    = module.tags_network.tags
 }
 
-resource "aws_subnet" "worker" {
+resource "aws_subnet" "worker_1" {
   vpc_id                  = aws_vpc.lab.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[1]
-  tags                    = module.tag_generator.tags
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  tags                    = module.tags_network.tags
 }
 
-resource "aws_subnet" "controlplane" {
+resource "aws_subnet" "worker_2" {
   vpc_id                  = aws_vpc.lab.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[1]
-  tags                    = module.tag_generator.tags
+  tags                    = module.tags_network.tags
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
+resource "aws_subnet" "controlplane_1" {
+  vpc_id                  = aws_vpc.lab.id
+  cidr_block              = "10.0.4.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  tags                    = module.tags_network.tags
 }
 
-resource "aws_key_pair" "lab_keypair" {
-  key_name   = format("%s%s", var.name, "_keypair")
-  public_key = file(var.public_key_path)
+resource "aws_subnet" "controlplane_2" {
+  vpc_id                  = aws_vpc.lab.id
+  cidr_block              = "10.0.5.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  tags                    = module.tags_network.tags
 }
 
-resource "aws_security_group" "alb-sec-group" {
-  name   = "alb-sec-group"
+resource "aws_security_group" "bastion" {
   vpc_id = aws_vpc.lab.id
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "controlplane" {
+  vpc_id = aws_vpc.lab.id
+
   egress {
     from_port   = 0
     protocol    = "-1"
@@ -68,21 +123,20 @@ resource "aws_security_group" "alb-sec-group" {
 
   ingress {
     from_port   = 80
-    protocol    = "tcp"
     to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    protocol    = "tcp"
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.bastion.id]
   }
 }
 
-resource "aws_security_group" "asg_sec_group" {
-  name   = "asg_sec_group"
+resource "aws_security_group" "worker" {
   vpc_id = aws_vpc.lab.id
 
   egress {
@@ -91,26 +145,38 @@ resource "aws_security_group" "asg_sec_group" {
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
-    from_port       = 80
+    from_port       = 22
+    to_port         = 22
     protocol        = "tcp"
-    to_port         = 80
-    security_groups = [aws_security_group.alb-sec-group.id]
+    security_groups = [aws_security_group.bastion.id]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.controlplane.id]
   }
 }
 
-resource "aws_launch_configuration" "ec2_template" {
-  image_id        = "ami-03b7e55086baf7a20"
+resource "aws_key_pair" "lab_keypair" {
+  key_name   = format("%s%s", var.name, "_keypair")
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_launch_configuration" "worker" {
+  image_id        = "ami-02c7c728a7874ae7a"
   instance_type   = "t3.micro"
-  user_data       = <<-EOF
-            #!/bin/bash
-            sudo apt update -y
-            sudo apt install -y httpd
-            echo "Website is Working !" > /var/www/html/index.html
-            systemctl start apache2
-            systemctl enable apache2
-            EOF
-  security_groups = [aws_security_group.asg_sec_group.id]
+  security_groups = [aws_security_group.worker.id]
   key_name        = aws_key_pair.lab_keypair.id
 
   lifecycle {
@@ -118,21 +184,21 @@ resource "aws_launch_configuration" "ec2_template" {
   }
 }
 
-resource "aws_autoscaling_group" "myasg" {
-  max_size                  = 5
+resource "aws_autoscaling_group" "workers" {
+  max_size                  = 3
   min_size                  = 2
-  launch_configuration      = aws_launch_configuration.ec2_template.name
+  launch_configuration      = aws_launch_configuration.worker.name
   health_check_grace_period = 300
 
-  health_check_type = "ELB"
+  health_check_type = "EC2"
 
-  vpc_zone_identifier = [aws_subnet.lab_subnet_1.id, aws_subnet.lab_subnet_2.id]
+  vpc_zone_identifier = [aws_subnet.worker_1.id, aws_subnet.worker_2.id]
 
   target_group_arns = [aws_lb_target_group.asg.arn]
 
   tag {
     key                 = "name"
-    value               = "myasg"
+    value               = "workers"
     propagate_at_launch = false
   }
 
@@ -142,32 +208,22 @@ resource "aws_autoscaling_group" "myasg" {
 }
 
 resource "aws_lb" "elb" {
-  name               = "terraform-asg-example"
+  name               = "workerselb"
   load_balancer_type = "application"
 
-  subnets         = [aws_subnet.lab_subnet_1.id, aws_subnet.lab_subnet_2.id]
-  security_groups = [aws_security_group.alb-sec-group.id]
+  subnets         = [aws_subnet.worker_1.id, aws_subnet.worker_2.id]
+  security_groups = [aws_security_group.worker.id]
 }
 
 resource "aws_lb_target_group" "asg" {
-  name     = "asg-example"
+  name     = "workertg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.lab.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
 }
 
 resource "aws_autoscaling_attachment" "asg" {
-  autoscaling_group_name = aws_autoscaling_group.myasg.id
+  autoscaling_group_name = aws_autoscaling_group.workers.id
   alb_target_group_arn   = aws_lb_target_group.asg.arn
 }
 
@@ -201,3 +257,32 @@ resource "aws_lb_listener_rule" "asg" {
     }
   }
 }
+
+resource "aws_instance" "controlplane_1" {
+  ami           = "ami-02c7c728a7874ae7a"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.controlplane_1.id
+	security_groups = [aws_security_group.controlplane.id]
+  key_name      = aws_key_pair.lab_keypair.id
+  tags          = module.tags_controlplane.tags
+}
+
+resource "aws_instance" "controlplane_2" {
+  ami           = "ami-02c7c728a7874ae7a"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.controlplane_2.id
+	security_groups = [aws_security_group.controlplane.id]
+  key_name      = aws_key_pair.lab_keypair.id
+  tags          = module.tags_controlplane.tags
+}
+
+resource "aws_instance" "bastion" {
+  ami           = "ami-02c7c728a7874ae7a"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.bastion.id
+	security_groups = [aws_security_group.bastion.id]
+  key_name      = aws_key_pair.lab_keypair.id
+  tags          = module.tags_bastion.tags
+}
+
+
